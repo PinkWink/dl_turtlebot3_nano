ebdff426b0746068ab81f83a854c9e6bd8fa5dad
PinkWink <pinkwink.korea@gmail.com>
2021-11-13 00:18:04 +0900

    Update robot_test.py

--- robot_code/robot_test.py
+++ robot_code/robot_test.py
@@ -1,11 +1,15 @@
 import serial
 import time
 import io
+import sys 
 
 ser = serial.Serial(port='/dev/ttyACM0', baudrate=57600)
 ser_io = io.TextIOWrapper(io.BufferedRWPair(ser, ser, 1), newline="\r", line_buffering = True)
 
+forward_vel = sys.argv[0]
+rotate_vel = sys.argv[1]
+
 while True:
-	command = "0,0\n"
+	command = str(forward_vel)+','+str(rotate_vel)+"\n"
 	ser.write(command.encode())
 	time.sleep(1)
