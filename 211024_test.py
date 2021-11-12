ba3d3e1dc486232dd3fbd6405b46cc19d25f8e9f
PinkWink <pinkwink.korea@gmail.com>
2021-11-12 10:23:03 +0900

    Update 211024_test.py

--- 211024_test.py
+++ 211024_test.py
@@ -5,6 +5,33 @@ import tensorflow as tf
 import serial
 import io
 
+def gstreamer_pipeline(
+    capture_width=320,
+    capture_height=180,
+    display_width=320, #1280,
+    display_height=180, #720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+    
 
 def drive(go, turn):
   command = str(go) + ',' + str(turn) + "\n"
